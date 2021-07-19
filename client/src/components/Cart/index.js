4a708bd1f9198e9b0a6dab8eb4968d0faad08002
Michelle <michey.asmah21@gmail.com>
2021-07-18 22:59:10 -0400

    cart selector and dispatch

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -8,6 +8,7 @@ import { idbPromise } from "../../utils/helpers";
 import { QUERY_CHECKOUT } from '../../utils/queries';
 import { loadStripe } from '@stripe/stripe-js';
 import { useLazyQuery } from '@apollo/client';
+import { useSelector, useDispatch } from 'react-redux'
 
 
 const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
@@ -15,7 +16,12 @@ const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
 
-  const [state, dispatch] = useStoreContext();
+  const state = useSelector((state) => {
+    return state
+  });
+  
+  const dispatch = useDispatch();
+
   const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
 
   useEffect(() => {
